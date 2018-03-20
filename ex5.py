553ab8fada477ba39090b98707744ffc50f5fda4
buckyhead2000 <buckyhead2000@yahoo.com>
2018-03-19 16:03:27 -1000

    bug fixes
    line 11 changed to . instead of ,
    comment out utf-8. had an error
    added closed parenthesis to print statements.
    added % on line 19.

--- ex5.py
+++ ex5.py
@@ -1,4 +1,4 @@
--*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 
 my_name = 'Zed A. Shaw'
 my_age = 35 #not a lie
@@ -8,12 +8,12 @@ my_eyes = 'Blue'
 my_teeth = 'White'
 my_hair = 'Brown'
 
-print("Let's talk about %s," % my_name)
-print("He's %d inches tall." % my_height
-print("He's %d pounds heavy." % my_weight
-print("Actually that's not too heavy."
+print("Let's talk about %s." % my_name)
+print("He's %d inches tall." % my_height)
+print("He's %d pounds heavy." % my_weight)
+print("Actually that's not too heavy.")
 print("He's got %s eyes and %s hair." % (my_eyes, my_hair))
-print("His teeth are usually %s depending on the coffee." % my_teeth
+print("His teeth are usually %s depending on the coffee." % my_teeth)
 
 # this line is tricky, try to get it exactly right
-print("If I add %d, %d, and %d I get %d." (my_age, my_height, my_weight, my_age + my_height + my_weight))
+print("If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
